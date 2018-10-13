34dd5db0c951784e78434156ee1270762f3ec469
Denis Zhdanov <denzhdanov@gmail.com>
2018-10-13 08:41:37 +0800

    Initial

---
+++ src/main/groovy/tech/harmonysoft/oss/gradle/dist/CustomGradleDistributionPlugin.groovy
@@ -0,0 +1,296 @@
+package tech.harmonysoft.oss.gradle.dist
+
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.tooling.BuildException
+
+import java.nio.channels.Channels
+import java.nio.charset.StandardCharsets
+import java.nio.file.FileSystem
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.util.regex.Pattern
+
+class CustomGradleDistExtension {
+    String gradleVersion
+    String customDistributionName
+    String customDistributionVersion
+    String gradleDistributionType = 'bin'
+}
+
+class CustomGradleDistributionPlugin implements Plugin<Project> {
+
+    private static final Pattern PATTERN = Pattern.compile(/\$([^\s]+)\$/)
+
+    @Override
+    void apply(Project project) {
+        def extension = project.extensions.create('gradleDist', CustomGradleDistExtension)
+
+        project.afterEvaluate {
+            validate(extension)
+
+            project.task('build').doLast {
+                def baseDistribution = getBaseGradleDistribution(project, extension)
+                def distributions = getDistributions(project)
+                if (distributions.empty) {
+                    prepareCustomDistribution(null, baseDistribution, project, extension)
+                } else {
+                    distributions.each { prepareCustomDistribution(it, baseDistribution, project, extension) }
+                }
+            }
+        }
+    }
+
+    private static void validate(CustomGradleDistExtension extension) {
+        if (extension.gradleVersion == null || extension.gradleVersion.trim().empty) {
+            throw new IllegalStateException(
+'''Can not build custom gradle distribution - base gradle version is undefined. Please specify it like below:
+
+    gradleDist {
+        gradleVersion = '4.10'
+    }''')}
+
+        if (extension.customDistributionVersion == null || extension.customDistributionVersion.trim().empty) {
+            throw new IllegalStateException(
+'''Can not build - custom gradle distribution version is undefined. Please specify it like below:
+
+    gradleDist {
+        customDistributionVersion = '1.0'
+    }''')}
+
+        if (extension.customDistributionName == null || extension.customDistributionName.trim().empty) {
+            throw new IllegalStateException(
+'''Can not build - custom gradle distribution project name is undefined. Please specify it like below:
+
+    gradleDist {
+        customDistributionName = 'my-project'
+    }''')}
+
+    }
+
+    private static Collection<String> getDistributions(Project project) {
+        def extensionRootDir = getExtensionsRootDir(project)
+        def childDirectories = extensionRootDir.listFiles({ it.directory } as FileFilter)
+        if (childDirectories == null || childDirectories.length == 0) {
+            project.logger.lifecycle('Using a single custom gradle distribution')
+            return []
+        } else {
+            def result = childDirectories.toList().collect { it.name }
+            project.logger.lifecycle("Using ${result.size()} custom gradle distributions: $result")
+            return result
+        }
+    }
+
+    private static File getExtensionsRootDir(Project project) {
+        return project.file('src/main/resources/init.d')
+    }
+
+    private static File getIncludeRootDir(Project project) {
+        return project.file('src/main/resources/include')
+    }
+
+
+    private static File getBaseGradleDistribution(Project project, CustomGradleDistExtension extension) {
+        try {
+            return doGetBaseGradleDistribution(project, extension)
+        } catch (Exception e) {
+            throw new BuildException("Failed to prepare base gradle distribution of version $extension.gradleVersion",
+                    e)
+        }
+    }
+
+    private static File doGetBaseGradleDistribution(Project project, CustomGradleDistExtension extension) {
+        def gradleBaseName = "gradle-$extension.gradleVersion"
+        def gradleZip = "$gradleBaseName-${extension.gradleDistributionType}.zip"
+        def baseGradleArchive = new File(project.buildDir, "download/$gradleZip")
+        if (!baseGradleArchive.file) {
+            def archiveDir = baseGradleArchive.parentFile
+            if (!archiveDir.directory) {
+                boolean ok = archiveDir.mkdirs()
+                if (!ok) {
+                    throw new IllegalStateException("Can't create directory $archiveDir.absolutePath to store "
+                            + "gradle distribution")
+                }
+            }
+            download(project, gradleZip, archiveDir)
+        }
+        return baseGradleArchive
+    }
+
+
+    private static void download(Project project, String archiveName, File archiveDir) {
+        def fromUrl = "https://services.gradle.org/distributions/$archiveName"
+        def from = Channels.newChannel(new URL(fromUrl).openStream())
+        def to = new File(archiveDir, archiveName)
+        project.logger.lifecycle("About to download a gradle distribution from $fromUrl to $to.absolutePath")
+        new FileOutputStream(to).withCloseable {
+            it.channel.transferFrom(from, 0, Long.MAX_VALUE)
+        }
+        project.logger.lifecycle("Downloaded a gradle distribution from $fromUrl to $to.absolutePath")
+    }
+
+    private static void prepareCustomDistribution(String distribution,
+                                                  File baseDistribution,
+                                                  Project project,
+                                                  CustomGradleDistExtension extension)
+    {
+        def gradlePart = "gradle-$extension.gradleVersion"
+        def customProjectPart = "$extension.customDistributionName-$extension.customDistributionVersion"
+        if (distribution != null) {
+            customProjectPart += "-$distribution"
+        }
+        def customDistributionFileName = "$gradlePart-${customProjectPart}.zip"
+        def customDistributionsDir = new File(project.buildDir, 'gradle-dist')
+        remove(customDistributionsDir)
+        Files.createDirectories(customDistributionsDir.toPath())
+        File result = new File(customDistributionsDir, customDistributionFileName)
+
+        copyBaseDistribution(baseDistribution, result)
+        addToZip(result, distribution, project, extension.gradleVersion)
+        project.logger.lifecycle("Prepared custom gradle distribution at $result.absolutePath")
+    }
+
+    private static void copyBaseDistribution(File baseDistribution, File customDistribution) {
+        try {
+            def from = new FileInputStream(baseDistribution).channel
+            def to = new FileOutputStream(customDistribution).channel
+            to.transferFrom(from, 0, Long.MAX_VALUE)
+        } catch (Exception e) {
+            throw new BuildException("Failed to copy base gradle distribution from $baseDistribution.absolutePath "
+                    + "to $customDistribution.absolutePath", e)
+        }
+    }
+
+    private static void remove(File toRemove) {
+        if (toRemove.directory) {
+            def children = toRemove.listFiles()
+            if (children != null) {
+                children.each { remove(it) }
+            }
+            def ok = toRemove.deleteDir()
+            if (!ok) {
+                throw new IllegalStateException("Failed to remove directory $toRemove.absolutePath")
+            }
+        } else if (toRemove.exists()) {
+            def ok = toRemove.delete()
+            if (!ok) {
+                throw new IllegalStateException("Failed to remove file $toRemove.absolutePath")
+            }
+        }
+    }
+
+    private static void addToZip(File zip, String distribution, Project project, String gradleVersion) {
+        try {
+            doAddToZip(zip, distribution, project, gradleVersion)
+        } catch (Exception e) {
+            throw new BuildException("Failed to add entries to the custom gradle distribution $zip.absolutePath", e)
+        }
+    }
+
+    private static void doAddToZip(File zip, String distribution, Project project, String gradleVersion) {
+        def extensionsRootDir = getExtensionsRootDir(project)
+        def zipFileSystem = FileSystems.newFileSystem(URI.create("jar:${zip.toPath().toUri()}"), ['create': 'true'])
+        if (distribution == null) {
+            addToZip(zipFileSystem, extensionsRootDir, project, gradleVersion)
+        } else {
+            addToZip(zipFileSystem, new File(extensionsRootDir, distribution), project, gradleVersion)
+        }
+        zipFileSystem.close()
+    }
+
+    private static void addToZip(FileSystem zip, File includeRootDir, Project project, String gradleVersion) {
+        addDirectoryToZip(zip, includeRootDir, includeRootDir, project, gradleVersion)
+    }
+
+    private static void addDirectoryToZip(FileSystem zip,
+                                          File includeRootDir,
+                                          File directoryToInclude,
+                                          Project project,
+                                          String gradleVersion)
+    {
+        def children = directoryToInclude.listFiles()
+        for (File child : children ) {
+            if (child.directory) {
+                addDirectoryToZip(zip, includeRootDir, child, project, gradleVersion)
+            } else {
+                addFileToZip(zip, includeRootDir, child, project, gradleVersion)
+            }
+        }
+    }
+
+    private static void addFileToZip(FileSystem zip,
+                                     File includeRootDir,
+                                     File fileToInclude,
+                                     Project project,
+                                     String gradleVersion)
+    {
+        def relativePath = fileToInclude.absolutePath.substring(includeRootDir.absolutePath.length())
+        def to = zip.getPath("gradle-$gradleVersion/init.d/$relativePath")
+        if (to.parent != null) {
+            Files.createDirectories(to.parent)
+        }
+        Files.copy(applyTemplates(fileToInclude, project).toPath(), to)
+        project.logger.lifecycle("Added $fileToInclude.absolutePath to the custom gradle distribution")
+    }
+
+    private static File applyTemplates(File file, Project project) {
+        def includeRootDir = getIncludeRootDir(project)
+        if (!includeRootDir.directory) {
+            project.logger.lifecycle("Skipped includes replacement - no 'include' directory is found "
+                    + "at $includeRootDir.absolutePath")
+            return file
+        }
+        def includes = [:].withDefault {
+            def include = new File(includeRootDir, "${it}.gradle")
+            if (include.file) {
+                return new String(Files.readAllBytes(include.toPath()))
+            } else {
+                return null
+            }
+        }
+
+        def text = new String(Files.readAllBytes(file.toPath()))
+        def matcher = PATTERN.matcher(text)
+        int start = 0
+        def buffer = new StringBuilder()
+        while (matcher.find()) {
+            def replaceFrom = matcher.group(1)
+            def replaceTo = includes.get(replaceFrom)
+            if (replaceTo == null) {
+                continue
+            }
+
+            buffer.append(text.substring(start, matcher.start()))
+            def indent = getIndent(text, matcher.start())
+            buffer.append(indentText(replaceTo.toString(), indent))
+            start = matcher.end()
+            project.logger.lifecycle("Applied replacement '$replaceFrom' to the $file.absolutePath")
+        }
+        if (start > 0) {
+            buffer.append(text.substring(start))
+        }
+
+        if (buffer.length() <= 0) {
+            return file
+        }
+
+        def result = Files.createTempFile("", "${file.name}.tmp")
+        return Files.write(result, buffer.toString().getBytes(StandardCharsets.UTF_8)).toFile()
+    }
+
+    private static int getIndent(String text, int offset) {
+        int result = 0
+        while (--offset > 0) {
+            if (text.charAt(offset) == '\n' as char) {
+                break
+            }
+            result++
+        }
+        return result
+    }
+
+    private static String indentText(String text, int indent) {
+        return text.replaceAll("\\n", "\n" + " " * indent)
+    }
+}
---
+++ src/test/groovy/tech/harmonysoft/oss/gradle/dist/CustomGradleDistributionPluginTest.groovy
@@ -0,0 +1,151 @@
+package tech.harmonysoft.oss.gradle.dist
+
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Test
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.FileSystem
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+
+import static java.util.function.Function.identity
+import static java.util.stream.Collectors.toMap
+import static org.junit.Assert.*
+
+class CustomGradleDistributionPluginTest {
+
+    private static final String GRADLE_VERSION = '4.10'
+    private static final String PROJECT_NAME = 'my-project'
+    private static final String PROJECT_VERSION = '1.0'
+
+    @Test
+    void 'when client project has a single distribution and no templates then it is correctly packaged'() {
+        doTest('single-distribution-no-templates')
+    }
+
+    private void doTest(String testName) {
+        def testFiles = prepareInput(testName)
+        GradleRunner.create()
+                    .withProjectDir(testFiles.inputRootDir)
+                    .withArguments('build')
+                    .withPluginClasspath()
+                    .withDebug(true)
+                    .build()
+        verify(testFiles.expectedRootDir, new File(testFiles.inputRootDir, 'build/gradle-dist'))
+    }
+
+    private TestFiles prepareInput(String testDir) {
+        def testRoot = new File(getClass().getResource("/$testDir").file)
+        def inputRootDir = copy(new File(testRoot, 'input'))
+        createGradleDistributionZip(inputRootDir)
+        return new TestFiles(inputRootDir, new File(testRoot, 'expected-init.d'))
+    }
+
+    private static File copy(File dir) {
+        def result = Files.createTempDirectory("${dir.name}-tmp").toFile()
+        def children = dir.listFiles()
+        for (child in children) {
+            copy(child, result)
+        }
+        return result
+    }
+
+    private static void copy(File toCopy, File destinationDir) {
+        if (toCopy.file) {
+            Files.copy(toCopy.toPath(), new File(destinationDir, toCopy.name).toPath())
+        } else {
+            def children = toCopy.listFiles()
+            if (children != null) {
+                def dir = new File(destinationDir, toCopy.name)
+                Files.createDirectories(dir.toPath())
+                for (child in children) {
+                    copy(child, dir)
+                }
+            }
+        }
+    }
+
+    private static void createGradleDistributionZip(File projectRootDir) {
+        def downloadDir = new File(projectRootDir, "build/download")
+        Files.createDirectories(downloadDir.toPath())
+        def zip = new File(downloadDir, "gradle-${GRADLE_VERSION}-bin.zip")
+        new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(zip))).withCloseable {
+            def entry = new ZipEntry("gradle-$GRADLE_VERSION/")
+            it.putNextEntry(entry)
+            it.closeEntry()
+        }
+    }
+
+    private static void verify(File expectedRootDir, File actualDistDir) {
+        def filter = { it.directory } as FileFilter
+        def expectedDistributions = expectedRootDir.listFiles(filter)
+        if (expectedDistributions.length == 0) {
+            assertEquals(actualDistDir.list().length, 1)
+            verify(expectedRootDir, getZipFs(actualDistDir, null))
+        }
+    }
+
+    private static FileSystem getZipFs(File parentDir, String distribution) {
+        def zipName = "gradle-$GRADLE_VERSION-$PROJECT_NAME-$PROJECT_VERSION"
+        if (distribution != null) {
+            zipName += "-$distribution"
+        }
+        zipName += '.zip'
+        def zip = new  File(parentDir, zipName)
+        assertTrue(zip.file)
+        return FileSystems.newFileSystem(URI.create("jar:${zip.toPath().toUri()}"), ['create': 'false'])
+    }
+
+    private static void verify(File expectedDir, FileSystem zipFs) {
+        verify(expectedDir, zipFs.getPath("gradle-$GRADLE_VERSION/init.d"))
+    }
+
+    private static void verify(File expectedDir, Path actualDir) {
+        def keyExtractor = { Path path ->
+            def pathAsString = path.toString()
+            def i = pathAsString.indexOf('init.d')
+            return pathAsString.substring(i + 'init.d/'.length())
+        }
+        Map<String, Path> allExpected = Files.list(expectedDir.toPath()).collect(toMap(keyExtractor, identity()))
+        Map<String, Path> allActual = Files.list(actualDir).collect(toMap(keyExtractor, identity()))
+
+        if (allExpected.size() != allActual.size()) {
+            def unexpected = new HashSet(allActual.keySet())
+            unexpected.removeAll(allActual.keySet())
+            if (!unexpected.empty) {
+                fail("Unexpected entries are found in the custom distribution's 'init.d' directory: $unexpected")
+            }
+
+            def unmatched = new HashSet(allExpected.keySet())
+            unmatched.removeAll(allActual.keySet())
+            if (!unmatched.empty) {
+                fail("Expected entries are not found in the custom distribution's 'init.d' directory: $unexpected")
+            }
+        }
+
+        allExpected.each { pathKey, path ->
+            if (!allActual.containsKey(pathKey)) {
+                fail("Expected to find '$pathKey' in the custom distribution's 'init.d' directory but it's not there")
+            }
+            if (path.toFile().file) {
+                def expectedText = new String(Files.readAllBytes(path), StandardCharsets.UTF_8)
+                def actualText = new String(Files.readAllBytes(allActual.get(pathKey)), StandardCharsets.UTF_8)
+                assertEquals("Text mismatch in $path", expectedText, actualText)
+            }
+        }
+    }
+
+    private static class TestFiles {
+
+        File inputRootDir
+        File expectedRootDir
+
+        TestFiles(File inputRootDir, File expectedRootDir) {
+            this.inputRootDir = inputRootDir
+            this.expectedRootDir = expectedRootDir
+        }
+    }
+}
